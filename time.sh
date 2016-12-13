3def0f13444d16010fb338b874d63c5ffe90f5f9
xnum <s000032001@gmail.com>
2016-12-13 14:31:37 +0800

    去掉多餘的字元

--- time.sh
+++ time.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# DIR=12-02_21:24; ./time.sh $DIR | tee $DIR/timeinfo.txt
+
 if [ $# -lt 1 ]; then
     echo "need dir"
     exit
@@ -7,5 +9,5 @@ fi
 
 dir=$1
 
-grep -rin "elapsed" $dir/*.log | awk '{split($1,arr,":");printf ("%s %8s\n",arr[2],substr($3,0,index($3,"elapsed")-1)) }' | awk '{ split($2,arr,":"); printf ("%-30s%g\n",$1,arr[1]*60+arr[2]) }' 
+grep -rin "elapsed" $dir/*.log | awk '{split($1,arr,":");printf ("%s %8s\n",substr(arr[2],4),substr($3,0,index($3,"elapsed")-1)) }' | awk '{ split($2,arr,":"); printf ("%-30s%g\n",$1,arr[1]*60+arr[2]) }' 
 
